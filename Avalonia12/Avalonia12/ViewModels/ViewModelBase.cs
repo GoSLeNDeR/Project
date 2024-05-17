370befad52b114745a714440710a569043bd816a
GoSLeNDeR <93986091+GoSLeNDeR@users.noreply.github.com>
2024-05-17 14:06:29 +0300

    Загрузка проекта
    загрузка

---
+++ Avalonia12/Avalonia12/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace Avalonia12.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ Avalonia12/Avalonia12/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Avalonia12.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
